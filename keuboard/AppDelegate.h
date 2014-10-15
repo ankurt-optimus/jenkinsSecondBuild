0a6e3a294160ce98f18682f2f1189b7f89eb1370
optimus-130 <optimus@optimuss-Mac-mini-5.local>
2014-10-15 15:11:08 +0530

    Initial Commit

---
+++ keuboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keuboard
+//
+//  Created by optimus-130 on 10/15/14.
+//  Copyright (c) 2014 optimus-130. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ keuboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  keuboard
+//
+//  Created by optimus-130 on 10/15/14.
+//  Copyright (c) 2014 optimus-130. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
